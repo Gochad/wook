768807423ef2cc04ca72877a717f22db7ada2306
Gochad <malaga.drag@gmail.com>
2023-04-12 23:54:42 +0200

    small code refactors and repair profile template

--- core/views.py
+++ core/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
-from django.views.generic import UpdateView
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from .models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
 from .forms import NewUserForm, UpdateUserForm, UpdateProfileForm
 
 
@@ -13,21 +12,21 @@ def dashboard(request):
 
 def home(request):
     if request.user.is_authenticated:
-        return redirect("dashboard")
+        return redirect('dashboard')
     return render(request, 'home.html', {})
 
 def register_request(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect("dashboard")
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+            messages.success(request, 'Registration successful')
+            return redirect('dashboard')
+        messages.error(request, 'Unsuccessful registration. Invalid information')
 
     form = NewUserForm()
-    return render(request, "register.html", context={"register_form": form})
+    return render(request, 'register.html', {'register_form': form})
 
 def login_request(request):
     if request.method == "POST":
@@ -35,40 +34,37 @@ def login_request(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(username, password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
-                return redirect("dashboard")
+                messages.info(request, f'You are now logged in as {username}')
+                return redirect('dashboard')
             else:
-                messages.error(request,"Invalid username or password.")
+                messages.error(request, 'Invalid username or password')
         else:
-            messages.error(request,"Invalid username or password.")
+            messages.error(request, 'Invalid username or password')
     form = AuthenticationForm()
-    return render(request, "login.html", context={"login_form":form})
+    return render(request, 'login.html', {'login_form': form})
 
 def logout_request(request):
     logout(request)
-    messages.info(request, "You have successfully logged out.")
-    return redirect("home")
+    messages.info(request, 'You have successfully logged out')
+    return redirect('home')
 
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         user_form = UpdateUserForm(request.POST, instance=request.user)
-#         profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
-#
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#             messages.success(request, 'Your profile is updated successfully')
-#             return redirect('core_profile')
-#     else:
-#         user_form = UpdateUserForm(instance=request.user)
-#         profile_form = UpdateProfileForm(instance=request.user.profile)
-#
-#     return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect('profile')
+    else:
+        user_form = UpdateUserForm(request.user)
+        profile_form = UpdateProfileForm(request.user.profile)
+
+    return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})
